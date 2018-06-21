9fbb97f4094c5ac13c199f2ccd0f9b7775e2fad3
0x5010 <0xh4cker@gmail.com>
2018-06-21 16:08:32 +0800

    license in code

--- tracker.go
+++ tracker.go
@@ -1,3 +1,13 @@
+/*
+ *
+ * Created by 0x5010 on 2018/06/20.
+ * grpcp
+ * https://github.com/0x5010/grpcp
+ *
+ * Copyright 2018 0x5010.
+ * Licensed under the MIT license.
+ *
+ */
 package grpcp
 
 import (
