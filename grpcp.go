120dcd121374603b3ea7ea8578353ec0a34518ff
0x5010 <0xh4cker@gmail.com>
2018-06-22 11:35:53 +0800

    grpcp

--- grpcp.go
+++ grpcp.go
@@ -12,10 +12,12 @@ package grpcp
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 )
 
 const (
@@ -128,4 +130,157 @@ func (ct *ConnectionTracker) Alives() []string {
 		alives = append(alives, addr)
 	}
 	return alives
+}
+
+type connState int
+
+const (
+	connecting connState = iota
+	ready
+	idle
+	shutdown
+)
+
+var (
+	errNoReady = fmt.Errorf("no ready")
+)
+
+type trackedConn struct {
+	sync.RWMutex
+	addr    string
+	conn    *grpc.ClientConn
+	tracker *ConnectionTracker
+	state   connState
+	expires time.Time
+	retry   int
+	cannel  context.CancelFunc
+}
+
+func (tc *trackedConn) tryconn(ctx context.Context) error {
+	tc.Lock()
+	defer tc.Unlock()
+	if tc.conn != nil {
+		if tc.state == ready {
+			return nil
+		}
+		if tc.state == idle {
+			return errNoReady
+		}
+	}
+	conn, err := tc.tracker.dial(tc.addr)
+	if err != nil {
+		return err
+	}
+	if tc.conn != nil {
+		tc.conn.Close()
+	}
+	tc.conn = conn
+
+	readyCtx, cancel := context.WithTimeout(ctx, tc.tracker.checkReadyTimeout)
+	defer cancel()
+
+	if ok := tc.isReady(readyCtx); !ok {
+		return errNoReady
+	}
+
+	hbCtx, cancel := context.WithCancel(ctx)
+	tc.cannel = cancel
+	go tc.heartbeat(hbCtx)
+	return nil
+}
+
+func (tc *trackedConn) getState() connState {
+	tc.RLock()
+	defer tc.RUnlock()
+	return tc.state
+}
+
+func (tc *trackedConn) healthCheck(ctx context.Context) {
+	tc.Lock()
+	defer tc.Unlock()
+	ctx, cancel := context.WithTimeout(ctx, tc.tracker.checkReadyTimeout)
+	defer cancel()
+
+	if ok := tc.isReady(ctx); !ok && tc.expired() {
+		tc.shutdown()
+	}
+}
+
+func (tc *trackedConn) isReady(ctx context.Context) bool {
+	for {
+		s := tc.conn.GetState()
+		if s == connectivity.Ready {
+			tc.ready()
+			return true
+		} else if s == connectivity.Shutdown {
+			tc.shutdown()
+			return false
+		}
+		if !tc.conn.WaitForStateChange(ctx, s) {
+			tc.idle()
+			return false
+		}
+	}
+}
+
+func (tc *trackedConn) ready() {
+	tc.state = ready
+	tc.expires = time.Now().Add(tc.tracker.timeout)
+	tc.retry = 0
+	tc.tracker.connReady(tc)
+}
+
+func (tc *trackedConn) idle() {
+	tc.state = idle
+	tc.retry++
+	tc.tracker.connUnReady(tc.addr)
+}
+
+func (tc *trackedConn) shutdown() {
+	tc.state = shutdown
+	tc.conn.Close()
+	tc.cannel()
+	tc.tracker.connUnReady(tc.addr)
+}
+
+func (tc *trackedConn) expired() bool {
+	return tc.expires.Before(time.Now())
+}
+
+func (tc *trackedConn) heartbeat(ctx context.Context) {
+	ticker := time.NewTicker(tc.tracker.heartbeatInterval)
+	for tc.getState() != shutdown {
+		select {
+		case <-ctx.Done():
+			tc.shutdown()
+			break
+		case <-ticker.C:
+			tc.healthCheck(ctx)
+		}
+	}
+}
+
+var (
+	// Pool default pool
+	Pool  *ConnectionTracker
+	dialF = func(addr string) (*grpc.ClientConn, error) {
+		return grpc.Dial(
+			addr,
+			grpc.WithInsecure(),
+		)
+	}
+)
+
+func init() {
+	Pool = New(dialF)
+}
+
+// GetConn create or get an existing connection from default pool
+func GetConn(addr string) (*grpc.ClientConn, error) {
+	return Pool.GetConn(addr)
+}
+
+// Alives current live connections from default pool
+func Alives() []string {
+	return Pool.Alives()
 }
